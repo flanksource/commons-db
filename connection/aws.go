de1b470ff8845a26e153fd33b423b169406d6762
Moshe Immerman <moshe@flanksource.com>
2025-10-15 15:07:52 +0300

    feat(connection): add Filesystem method to S3Connection

---
+++ connection/aws.go
@@ -0,0 +1,177 @@
+package connection
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/flanksource/commons-db/context"
+	"github.com/flanksource/commons-db/models"
+	"github.com/flanksource/commons-db/types"
+	"github.com/henvic/httpretty"
+)
+
+// +kubebuilder:object:generate=true
+type AWSConnection struct {
+	// ConnectionName of the connection. It'll be used to populate the endpoint, accessKey and secretKey.
+	ConnectionName string       `yaml:"connection,omitempty" json:"connection,omitempty"`
+	AccessKey      types.EnvVar `yaml:"accessKey,omitempty" json:"accessKey,omitempty"`
+	SecretKey      types.EnvVar `yaml:"secretKey,omitempty" json:"secretKey,omitempty"`
+	SessionToken   types.EnvVar `yaml:"sessionToken,omitempty" json:"sessionToken,omitempty"`
+	AssumeRole     string       `yaml:"assumeRole,omitempty" json:"assumeRole,omitempty"`
+	Region         string       `yaml:"region,omitempty" json:"region,omitempty"`
+	Endpoint       string       `yaml:"endpoint,omitempty" json:"endpoint,omitempty"`
+	// Skip TLS verify when connecting to aws
+	SkipTLSVerify bool `yaml:"skipTLSVerify,omitempty" json:"skipTLSVerify,omitempty"`
+}
+
+func (t *AWSConnection) GetUsername() types.EnvVar {
+	return t.AccessKey
+}
+
+func (t *AWSConnection) GetPassword() types.EnvVar {
+	return t.SecretKey
+}
+
+func (t *AWSConnection) GetProperties() map[string]string {
+	return map[string]string{
+		"region": t.Region,
+	}
+}
+
+func (t *AWSConnection) GetURL() types.EnvVar {
+	return types.EnvVar{ValueStatic: t.Endpoint}
+}
+
+func (t *AWSConnection) FromModel(connection models.Connection) {
+	t.ConnectionName = connection.Name
+	t.AccessKey = types.EnvVar{ValueStatic: connection.Username}
+	t.SecretKey = types.EnvVar{ValueStatic: connection.Password}
+	t.Endpoint = connection.URL
+	t.SkipTLSVerify = connection.InsecureTLS
+	if region, ok := connection.Properties["region"]; ok {
+		t.Region = region
+	}
+	if assumeRole, ok := connection.Properties["assumeRole"]; ok {
+		t.AssumeRole = assumeRole
+	}
+}
+
+func (t AWSConnection) ToModel() models.Connection {
+	return models.Connection{
+		Type:        models.ConnectionTypeAWS,
+		Username:    t.AccessKey.ValueStatic,
+		Password:    t.SecretKey.ValueStatic,
+		URL:         t.Endpoint,
+		InsecureTLS: t.SkipTLSVerify,
+		Properties: types.JSONStringMap{
+			"region":     t.Region,
+			"assumeRole": t.AssumeRole,
+		},
+	}
+}
+
+// Populate populates an AWSConnection with credentials and other information.
+// If a connection name is specified, it'll be used to populate the endpoint, accessKey and secretKey.
+func (t *AWSConnection) Populate(ctx ConnectionContext) error {
+	if t.ConnectionName != "" {
+		connection, err := ctx.HydrateConnectionByURL(t.ConnectionName)
+		if err != nil {
+			return fmt.Errorf("could not parse EC2 access key: %w", err)
+		}
+		if connection == nil {
+			return fmt.Errorf("connection[%s] not found", t.ConnectionName)
+		}
+
+		t.AccessKey.ValueStatic = connection.Username
+		t.SecretKey.ValueStatic = connection.Password
+		if t.Endpoint == "" {
+			t.Endpoint = connection.URL
+		}
+
+		t.SkipTLSVerify = connection.InsecureTLS
+		if t.Region == "" {
+			if region, ok := connection.Properties["region"]; ok {
+				t.Region = region
+			}
+		}
+
+		if t.AssumeRole == "" {
+			if role, ok := connection.Properties["assumeRole"]; ok {
+				t.AssumeRole = role
+			}
+		}
+	}
+
+	if accessKey, err := ctx.GetEnvValueFromCache(t.AccessKey, ctx.GetNamespace()); err != nil {
+		return fmt.Errorf("could not get AWS access key id from env var: %w", err)
+	} else {
+		t.AccessKey.ValueStatic = accessKey
+	}
+
+	if secretKey, err := ctx.GetEnvValueFromCache(t.SecretKey, ctx.GetNamespace()); err != nil {
+		return fmt.Errorf("could not get AWS secret access key from env var: %w", err)
+	} else {
+		t.SecretKey.ValueStatic = secretKey
+	}
+
+	if sessionToken, err := ctx.GetEnvValueFromCache(t.SessionToken, ctx.GetNamespace()); err != nil {
+		return fmt.Errorf("could not get AWS session token from env var: %w", err)
+	} else {
+		t.SessionToken.ValueStatic = sessionToken
+	}
+
+	return nil
+}
+
+// Client returns a new aws config.
+// Call this on a hydrated connection.
+func (t *AWSConnection) Client(ctx context.Context) (aws.Config, error) {
+	var tr http.RoundTripper
+	tr = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: t.SkipTLSVerify},
+	}
+
+	if ctx.IsTrace() {
+		httplogger := &httpretty.Logger{
+			Time:           true,
+			TLS:            ctx.Logger.IsLevelEnabled(7),
+			RequestHeader:  true,
+			RequestBody:    ctx.Logger.IsLevelEnabled(8),
+			ResponseHeader: true,
+			ResponseBody:   ctx.Logger.IsLevelEnabled(9),
+			Colors:         true,
+			Formatters:     []httpretty.Formatter{&httpretty.JSONFormatter{}},
+		}
+
+		tr = httplogger.RoundTripper(tr)
+	}
+
+	options := []func(*config.LoadOptions) error{
+		config.WithHTTPClient(&http.Client{Transport: tr}),
+	}
+
+	if t.Region != "" {
+		options = append(options, config.WithRegion(t.Region))
+	}
+
+	if !t.AccessKey.IsEmpty() {
+		options = append(options, config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(t.AccessKey.ValueStatic, t.SecretKey.ValueStatic, "")))
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx, options...)
+	if err != nil {
+		return aws.Config{}, err
+	}
+
+	if t.AssumeRole != "" {
+		cfg.Credentials = aws.NewCredentialsCache(stscreds.NewAssumeRoleProvider(sts.NewFromConfig(cfg), t.AssumeRole))
+	}
+
+	return cfg, nil
+}
---
+++ connection/s3.go
@@ -0,0 +1,90 @@
+package connection
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/flanksource/commons/collections"
+	"github.com/flanksource/commons-db/context"
+	"github.com/flanksource/commons-db/fs"
+	"github.com/flanksource/commons-db/models"
+)
+
+// +kubebuilder:object:generate=true
+type S3Connection struct {
+	AWSConnection `json:",inline"`
+	Bucket        string `yaml:"bucket,omitempty" json:"bucket,omitempty"`
+	// glob path to restrict matches to a subset
+	ObjectPath string `yaml:"objectPath,omitempty" json:"objectPath,omitempty"`
+	// Use path style path: http://s3.amazonaws.com/BUCKET/KEY instead of http://BUCKET.s3.amazonaws.com/KEY
+	UsePathStyle bool `yaml:"usePathStyle,omitempty" json:"usePathStyle,omitempty"`
+}
+
+func (t *S3Connection) GetProperties() map[string]string {
+	return collections.MergeMap(
+		t.AWSConnection.GetProperties(),
+		map[string]string{
+			"bucket":       t.Bucket,
+			"objectPath":   t.ObjectPath,
+			"usePathStyle": strconv.FormatBool(t.UsePathStyle),
+		})
+}
+
+// Populate populates an AWSConnection with credentials and other information.
+// If a connection name is specified, it'll be used to populate the endpoint, accessKey and secretKey.
+func (t *S3Connection) Populate(ctx ConnectionContext) error {
+	if err := t.AWSConnection.Populate(ctx); err != nil {
+		return err
+	}
+
+	if t.ConnectionName != "" {
+		connection, err := ctx.HydrateConnectionByURL(t.ConnectionName)
+		if err != nil {
+			return fmt.Errorf("could not parse EC2 access key: %v", err)
+		}
+
+		if region, ok := connection.Properties["bucket"]; ok {
+			t.Bucket = region
+		}
+
+		if objectPath, ok := connection.Properties["objectPath"]; ok {
+			t.ObjectPath = objectPath
+		}
+
+		if usePathStyle, ok := connection.Properties["usePathStyle"]; ok {
+			if val, err := strconv.ParseBool(usePathStyle); err == nil {
+				t.UsePathStyle = val
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (c S3Connection) ToModel() models.Connection {
+	conn := c.AWSConnection.ToModel()
+	conn.Type = models.ConnectionTypeS3
+	if c.Bucket != "" {
+		conn.Properties["bucket"] = c.Bucket
+	}
+	if c.ObjectPath != "" {
+		conn.Properties["objectPath"] = c.ObjectPath
+	}
+	if c.UsePathStyle {
+		conn.Properties["usePathStyle"] = strconv.FormatBool(c.UsePathStyle)
+	}
+	return conn
+}
+
+// Filesystem returns a filesystem interface for S3 operations
+func (c *S3Connection) Filesystem(ctx context.Context) (fs.FilesystemRW, error) {
+	if err := c.Populate(ctx); err != nil {
+		return nil, err
+	}
+	cfg, err := c.Client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return fs.NewS3FS(ctx, c.Bucket, cfg), nil
+}
