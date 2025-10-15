1c06975294a135ef266cc0a49e432ca3e1ce4b82
Moshe Immerman <moshe@flanksource.com>
2025-10-15 15:08:27 +0300

    feat(connection): add Filesystem method to GCSConnection

---
+++ connection/gcp.go
@@ -0,0 +1,117 @@
+package connection
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+
+	"github.com/flanksource/commons/hash"
+	"github.com/flanksource/commons/utils"
+	"github.com/flanksource/commons-db/context"
+	"github.com/flanksource/commons-db/models"
+	"github.com/flanksource/commons-db/types"
+)
+
+// +kubebuilder:object:generate=true
+type GCPConnection struct {
+	// ConnectionName of the connection. It'll be used to populate the endpoint and credentials.
+	ConnectionName string        `yaml:"connection,omitempty" json:"connection,omitempty"`
+	Endpoint       string        `yaml:"endpoint" json:"endpoint,omitempty"`
+	Credentials    *types.EnvVar `yaml:"credentials" json:"credentials,omitempty"`
+
+	// Skip TLS verify
+	SkipTLSVerify bool `yaml:"skipTLSVerify,omitempty" json:"skipTLSVerify,omitempty"`
+
+	Project string `yaml:"project" json:"project,omitempty"`
+}
+
+func (t *GCPConnection) ToModel() models.Connection {
+	return models.Connection{
+		Name:        t.ConnectionName,
+		URL:         t.Endpoint,
+		Certificate: t.Credentials.String(),
+		InsecureTLS: t.SkipTLSVerify,
+	}
+}
+
+func (t *GCPConnection) FromModel(connection models.Connection) {
+	t.ConnectionName = connection.Name
+	t.Credentials = &types.EnvVar{ValueStatic: connection.Certificate}
+	t.Endpoint = connection.URL
+	t.SkipTLSVerify = connection.InsecureTLS
+}
+
+func (g *GCPConnection) TokenSource(ctx context.Context, scopes ...string) (oauth2.TokenSource, error) {
+	creds, err := google.CredentialsFromJSON(ctx, []byte(g.Credentials.ValueStatic), scopes...)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenSource := creds.TokenSource
+	return tokenSource, nil
+}
+
+func (g *GCPConnection) Validate() *GCPConnection {
+	if g == nil {
+		return &GCPConnection{}
+	}
+
+	return g
+}
+
+func (g *GCPConnection) Token(ctx context.Context, freshToken bool, scopes ...string) (*oauth2.Token, error) {
+	cacheKey := tokenCacheKey("gcp", hash.Sha256Hex(g.Credentials.ValueStatic), strings.Join(scopes, ","))
+	if !freshToken {
+		if found, ok := tokenCache.Get(cacheKey); ok {
+			return found.(*oauth2.Token), nil
+		}
+	}
+
+	tokenSource, err := g.TokenSource(ctx, scopes...)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	tokenCache.Set(cacheKey, token, time.Until(token.Expiry)-tokenSafetyMargin)
+	return token, nil
+}
+
+// HydrateConnection attempts to find the connection by name
+// and populate the endpoint and credentials.
+func (g *GCPConnection) HydrateConnection(ctx ConnectionContext) error {
+	connection, err := ctx.HydrateConnectionByURL(g.ConnectionName)
+	if err != nil {
+		return err
+	}
+
+	if connection != nil {
+		g.Credentials = &types.EnvVar{ValueStatic: connection.Certificate}
+		g.Endpoint = connection.URL
+	}
+
+	if g.Credentials != nil {
+		if cred, err := ctx.GetEnvValueFromCache(*g.Credentials, ctx.GetNamespace()); err != nil {
+			return fmt.Errorf("could not get GCP credentials from env var: %w", err)
+		} else {
+			g.Credentials.ValueStatic = cred
+		}
+	}
+
+	return nil
+}
+
+func (t *GCPConnection) GetCertificate() types.EnvVar {
+	return utils.Deref(t.Credentials)
+}
+
+func (t *GCPConnection) GetURL() types.EnvVar {
+	return types.EnvVar{ValueStatic: t.Endpoint}
+}
---
+++ connection/gcs.go
@@ -0,0 +1,102 @@
+package connection
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	gcs "cloud.google.com/go/storage"
+	"github.com/flanksource/commons-db/context"
+	"github.com/flanksource/commons-db/fs"
+	"github.com/flanksource/commons-db/types"
+	"google.golang.org/api/option"
+)
+
+// +kubebuilder:object:generate=true
+type GCSConnection struct {
+	GCPConnection `json:",inline"`
+	Bucket        string `yaml:"bucket,omitempty" json:"bucket,omitempty"`
+}
+
+func (g *GCSConnection) Validate() *GCSConnection {
+	if g == nil {
+		return &GCSConnection{}
+	}
+
+	return g
+}
+
+func (g *GCSConnection) Client(ctx context.Context) (*gcs.Client, error) {
+	g = g.Validate()
+	var client *gcs.Client
+	var err error
+
+	var clientOpts []option.ClientOption
+
+	if g.Endpoint != "" {
+		clientOpts = append(clientOpts, option.WithEndpoint(g.Endpoint))
+	}
+
+	if g.SkipTLSVerify {
+		insecureHTTPClient := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+
+		clientOpts = append(clientOpts, option.WithHTTPClient(insecureHTTPClient))
+	}
+
+	if g.Credentials != nil && !g.Credentials.IsEmpty() {
+		credential, err := ctx.GetEnvValueFromCache(*g.Credentials, ctx.GetNamespace())
+		if err != nil {
+			return nil, err
+		}
+		clientOpts = append(clientOpts, option.WithCredentialsJSON([]byte(credential)))
+	} else {
+		clientOpts = append(clientOpts, option.WithoutAuthentication())
+	}
+
+	client, err = gcs.NewClient(ctx.Context, clientOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// HydrateConnection attempts to find the connection by name
+// and populate the endpoint and credentials.
+func (g *GCSConnection) HydrateConnection(ctx ConnectionContext) error {
+	connection, err := ctx.HydrateConnectionByURL(g.ConnectionName)
+	if err != nil {
+		return err
+	}
+
+	if connection != nil {
+		g.Credentials = &types.EnvVar{ValueStatic: connection.Certificate}
+		g.Endpoint = connection.URL
+		if val, ok := connection.Properties["bucket"]; ok {
+			g.Bucket = val
+		}
+	}
+
+	return nil
+}
+
+func (t *GCSConnection) GetProperties() map[string]string {
+	return map[string]string{
+		"bucket": t.Bucket,
+	}
+}
+
+// Filesystem returns a filesystem interface for GCS operations
+func (c *GCSConnection) Filesystem(ctx context.Context) (fs.FilesystemRW, error) {
+	if err := c.HydrateConnection(ctx); err != nil {
+		return nil, err
+	}
+	client, err := c.Client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return fs.NewGCSFS(ctx, c.Bucket, client), nil
+}
