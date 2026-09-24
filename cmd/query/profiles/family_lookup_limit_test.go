9dd5fb948e0af7e6cbddacbd8f8910848c10433c
Moshe Immerman <moshe@flanksource.com>
2026-09-24 22:03:58 +0300

    refactor(api): Refactor RegisterRoutes to accept router instead of mux

--- cmd/query/profiles/family_lookup_limit_test.go
+++ cmd/query/profiles/family_lookup_limit_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/flanksource/clicky/entity"
+	"github.com/flanksource/clicky/route"
 	"github.com/flanksource/clicky/rpc"
 	dbcontext "github.com/flanksource/commons-db/context"
 	"github.com/flanksource/commons-db/query"
@@ -96,7 +97,7 @@ func newCountingFamilyTest(t *testing.T, distinct int, profiles ...query.Profile
 		&rpc.OpenAPIConfig{Title: "Query", Version: "0.1.0"},
 	)
 	mux := http.NewServeMux()
-	server.RegisterRoutes(mux)
+	server.RegisterRoutes(route.NewRouter(mux))
 	return mux, provider
 }
 
